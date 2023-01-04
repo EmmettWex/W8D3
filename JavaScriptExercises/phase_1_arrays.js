f45b884ea3ee970b689cfb1892e0fb28e62c7b3b
Kin Ka Tse <kinkatse@Kins-MacBook-Air.local>
2023-01-04 15:42:49 -0500

    Finished phase 1

--- JavaScriptExercises/phase_1_arrays.js
+++ JavaScriptExercises/phase_1_arrays.js
@@ -23,3 +23,44 @@ Array.prototype.uniq = function() {
     return finalArr;
 };
 
+Array.prototype.twoSum = function(){
+    const pair = [];
+
+    for (let i = 0; i < this.length; i++) {
+        const num1 = this[i];
+        for (let j = i+1; j < this.length; j++) {
+            const num2 = this[j];
+            if (num1 + num2 === 0){
+                pair.push([i, j]);
+            }
+        }
+    }
+
+    return pair;
+}
+
+// console.log([2,-2,1,1,1,1,1,4,67,3,-1].twoSum());
+
+Array.prototype.transpose = function(){
+    let tMatrix = [];
+    let tempArr;
+
+    for (let i = 0; i < this[0].length; i++) {
+        tempArr = [];
+
+        for (let j = 0; j < this.length; j++) {
+            const num = this[j][i];
+            tempArr.push(num);
+        }
+        tMatrix.push(tempArr);
+    }
+
+    return tMatrix;
+}
+
+let arr = [
+    [1,2,3],
+    [4,5,6]
+]
+
+console.log(arr.transpose());
